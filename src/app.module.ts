ef0a3bdd9023d618c0353ad8a4f8532700e3c93f
Musheer0 <147691647+Musheer0@users.noreply.github.com>
2025-09-04 02:22:31 +0530

    created auth-users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthUsersModule } from './auth-users/auth-users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthUsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
