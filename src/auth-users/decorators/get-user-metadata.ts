8615bdde23231cf8186d22dbdcaa5808c8860693
Musheer0 <147691647+Musheer0@users.noreply.github.com>
2025-09-04 02:39:16 +0530

    created user metada decorator

---
+++ src/auth-users/decorators/get-user-metadata.ts
@@ -0,0 +1,19 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { Request } from "express";
+import { UserMetaData } from "src/utils/types/user-metadata";
+
+export const GetUserMetaDate = createParamDecorator((data:unknown,ctx:ExecutionContext)=>{
+    const req =ctx.switchToHttp().getRequest<Request>()
+     const forwarded = req.headers['x-forwarded-for'];
+     const ip =
+     typeof forwarded === 'string'
+        ? forwarded.split(',')[0].trim()
+        : req.socket?.remoteAddress;
+    const user_agent =req.headers['user-agent']
+
+    return {
+        ip,
+        user_agent
+    } as UserMetaData
+
+})
