a91c062906f006057d4d4b2b5a13f40a4d35de99
Musheer0 <147691647+Musheer0@users.noreply.github.com>
2025-09-04 16:09:02 +0530

    created public  only guard

--- src/auth-users/auth-users.controller.ts
+++ src/auth-users/auth-users.controller.ts
@@ -1,16 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { SignUpEmailDto } from './dto/sign-up/sign-up-email.dto';
 import { GetUserMetaData } from 'src/decorators/get-user-metadata';
 import { UserMetaData } from 'src/utils/types/user-metadata';
 import { AuthUsersService } from './auth-users.service';
 import { VerifyTokenDto } from './dto/verify-token/verify-token.dto';
 import { CredentialsSignInDto } from './dto/sign-in/credentials-sign-in.dto';
+import { PublicOnlyGuard } from './guards/public-only.guard';
 
 @Controller('api/auth')
 export class AuthUsersController {
     constructor(
         private readonly AuthService:AuthUsersService
     ){}
+    @UseGuards(PublicOnlyGuard)
     @Post('/sign-up/email')
     async SignUpUser(@Body() data:SignUpEmailDto,@GetUserMetaData() metadata:UserMetaData){
        return this.AuthService.SignUpWithEmail(metadata,data)
@@ -20,6 +22,7 @@ export class AuthUsersController {
     async VerifyUser(@Body() data:VerifyTokenDto,@GetUserMetaData() metadata:UserMetaData){
        return this.AuthService.VerifyUserEmail(metadata,data)
     }
+   @UseGuards(PublicOnlyGuard)
     @Post('/sign-in/email')
     async CredentialsSigin(@Body() data:CredentialsSignInDto,@GetUserMetaData() metadata:UserMetaData){
        return this.AuthService.CredentialsUserSign(metadata,data)
