51532e941c0cef198c39590b7184ae5e7831dfb6
Musheer0 <147691647+Musheer0@users.noreply.github.com>
2025-09-04 14:21:28 +0530

    added jwt

--- src/auth-users/auth-users.module.ts
+++ src/auth-users/auth-users.module.ts
@@ -2,10 +2,28 @@ import { Module } from '@nestjs/common';
 import { AuthUsersService } from './auth-users.service';
 import { AuthUsersController } from './auth-users.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy/jwt-strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   providers: [AuthUsersService],
   controllers: [AuthUsersController],
-  imports:[PrismaModule]
+  imports:[PrismaModule,
+    JwtModule.registerAsync({
+      useFactory:async()=>({
+        secret:process.env.AUTH_SECRET||'JWT_SECRET',
+        signOptions:{
+          expiresIn:'14d'
+        }
+      })
+    })
+
+  ],
+  exports:[
+    JwtStrategy,
+    PassportModule,
+    
+  ]
 })
 export class AuthUsersModule {}
