837c14b0a3cf43d3ea1e714f87a768aa1af3b019
Musheer0 <147691647+Musheer0@users.noreply.github.com>
2025-09-04 11:54:06 +0530

    added redis

--- src/auth-users/services/get-user-by-email.service.ts
+++ src/auth-users/services/get-user-by-email.service.ts
@@ -1,9 +1,22 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient, user } from "@prisma/client"
+import { redis } from "src/utils/others/redis";
 
 export const GetUserByEmail = async(prisma:PrismaClient,email:string)=>{
-    return prisma.user.findUnique({
+    const cache:user|null = await redis.get(email)
+    if(cache){
+        return cache
+    }
+
+    const exiting_user =  await prisma.user.findUnique({
         where:{
             email
         }
-    })
+    });
+    if(exiting_user){
+        await redis.set(email,exiting_user,{ex:24*60*60})
+        await redis.set(exiting_user?.id,exiting_user,{ex:24*60*60})
+        return exiting_user
+    }
+    return null
+
 }
