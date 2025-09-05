820be04e7cea835eb711bbab0c78969111e2d089
Musheer0 <147691647+Musheer0@users.noreply.github.com>
2025-09-05 11:32:08 +0530

    added rate limit , readme.mmd

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthUsersModule } from './auth-users/auth-users.module';
+import { Throttle, ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [PrismaModule, AuthUsersModule],
+  imports: [PrismaModule, AuthUsersModule,
+    ThrottlerModule.forRoot({
+      throttlers:[
+        {
+          ttl:6000,
+          limit:5
+        }
+      ]
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide:APP_GUARD,
+      useClass:ThrottlerGuard
+    }
+  ],
 })
 export class AppModule {}
--- src/auth-users/auth-users.controller.ts
+++ src/auth-users/auth-users.controller.ts
@@ -12,39 +12,45 @@ import { DisableMfaDto } from './dto/disable-mfa/disable-mfa.token';
 import { ResendTokenDto } from './dto/verify-token/resend-token.dto';
 import { EditUserBasicInfoDto } from './dto/edit-user/edit-user-basic-info.dto';
 import { EditUserEmailDto } from './dto/edit-user/edit-email.dto';
+import { Throttle } from '@nestjs/throttler';
 
 @Controller('api/auth')
 export class AuthUsersController {
     constructor(
         private readonly AuthService:AuthUsersService
     ){}
+    @Throttle({default:{ttl:60000,limit:4}})
     @UseGuards(PublicOnlyGuard)
     @Post('/sign-up/email')
     async SignUpUser(@Body() data:SignUpEmailDto,@GetUserMetaData() metadata:UserMetaData){
        return this.AuthService.SignUpWithEmail(metadata,data)
     }
+    @Throttle({default:{ttl:60000,limit:2}})
     @UseGuards(PublicOnlyGuard)
     @Post('/verify/email')
     async VerifyUser(@Body() data:VerifyTokenDto,@GetUserMetaData() metadata:UserMetaData){
        return this.AuthService.VerifyUserEmail(metadata,data)
     }
+    @Throttle({default:{ttl:60000,limit:1}})
     @UseGuards(PublicOnlyGuard)
     @Post('/resend/verify/email')
     async ResendVerifyEmail(@Body() data:ResendTokenDto,@GetUserMetaData() metadata:UserMetaData){
        return this.AuthService.ResendEmailVerificationMail(metadata,data)
     }
-
+   @Throttle({default:{ttl:60000,limit:4}})
    @UseGuards(PublicOnlyGuard)
     @Post('/sign-in/email')
     async CredentialsSigin(@Body() data:CredentialsSignInDto,@GetUserMetaData() metadata:UserMetaData){
        return this.AuthService.CredentialsUserSign(metadata,data)
-    }
+    }   
+    @Throttle({default:{ttl:60000*30,limit:4}})
     @UseGuards(JWTGuard)
-    @Post('/enable/mfa')
+    @Post('/enable/mfa') 
     async EnableMfa(@Request() req,){
       const session:UserSessionToken = req['user']
       return this.AuthService.EnableUserMfa(session)
     }
+    @Throttle({default:{ttl:60000*60,limit:5}})
     @UseGuards(JWTGuard)
     @Post('/disable/mfa')
     async DisableMfa(@Request() req,@GetUserMetaData() metadata:UserMetaData,@Body() data:DisableMfaDto,){
@@ -52,32 +58,35 @@ export class AuthUsersController {
       
       return this.AuthService.DisableUserMfa(session,metadata,data)
     }
+    @Throttle({default:{ttl:60000*5,limit:2}})
     @UseGuards(JWTGuard)
     @Patch('/edit/basic')
     async BasicEdit(@Request() req,@GetUserMetaData() metadata:UserMetaData,@Body() data:EditUserBasicInfoDto,){
       const session:UserSessionToken = req['user']
       return this.AuthService.EditUserBasicInfo(session.user_id,data,session)
     }
-    
+    @Throttle({default:{ttl:60000*60*7,limit:4}})
     @UseGuards(JWTGuard)
     @Patch('/edit/email')
     async EmailEdit(@Request() req,@GetUserMetaData() metadata:UserMetaData,@Body() data:EditUserEmailDto,){
       const session:UserSessionToken = req['user']
       return this.AuthService.EditUserEmailInfo(metadata,data,session)
     }
+    @Throttle({default:{ttl:1500,limit:1}})
     @UseGuards(JWTGuard)
     @Post('/user/me')
     async GetSessionInfo(@Request() req){
       const session:UserSessionToken = req['user']
       return this.AuthService.GetSessionInfo(session)
     }
-      @UseGuards(JWTGuard)
+    @Throttle({default:{ttl:30000,limit:1}})
+    @UseGuards(JWTGuard)
     @Delete('/user/logout')
     async Logout(@Request() req){
       const session:UserSessionToken = req['user']
       return this.AuthService.LogoutUser(session)
     }
-    
+    @Throttle({default:{ttl:60000,limit:1}})
     @UseGuards(JWTGuard)
     @Delete('/user/logout-all')
     async LogoutAll(@Request() req){
